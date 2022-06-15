c5117e695dc62e59c6115de8a5e7b2af77aa9f75
Tanishk-Saxena <tanishksaxena686@outlook.com>
2022-06-15 21:43:19 +0530

    initial commit - first draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import TextForm from './components/TextForm';
+import React, {useState} from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  const toggleMode = () => {
+    if(mode==="dark"){
+      setMode("light");
+      document.body.style.backgroundColor="white";
+      showAlert("Dark Mode is disabled.", "success");
+      document.title="TextUtils - Light Mode";
+    }else{
+      setMode("dark");
+      document.body.style.backgroundColor="black";
+      showAlert("Dark Mode is enabled.", "success");
+      document.title="TextUtils - Dark Mode";
+      // setInterval(() => {
+      //   document.title="TextUtils is amazing!";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title="Download TextUtils now!";
+      // }, 1500);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router> 
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <div className="container my-3"> 
+      <Switch>
+          {/* /users --> Component 1
+          /users/home --> Component 2
+          React router dom uses partial matching unless told otherwise by using the keyword "exact" with path
+          This means that if we don't use exact, and go to second path, still component 1 will be rendered.
+          Thus, to avoid that we use "exact" */}
+          <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/>}/>
+        </Switch>
+      </div>
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const handleOnUpperClick = () => {
+        // console.log("Uppercase was clicked: " + text);
+        let newText=text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to UPPER CASE", "success");
+    }
+    const handleOnLowerClick = () => {
+        let newText=text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to lower case", "success");
+    }
+    const handleOnClearClick = () => {
+        let newText="";
+        setText(newText);
+        props.showAlert("Cleared the input", "success");
+    }
+    const handleOnAlternatingClick = () => {
+        let newText="";
+        for(var i=0; i<text.length; i+=2){
+            newText+=text.charAt(i).toLowerCase();
+            newText+=text.charAt(i+1).toUpperCase();
+        }
+        //below code is so that every word in alternating case starts from lowercase and spaces are not counted while transforming
+        // for(var i=0; i<wordsArray.length; i++){
+        //     for(var j=0; j<wordsArray[i].length; j+=2){
+        //         newText+=wordsArray[i].charAt(j).toLowerCase();
+        //         newText+=wordsArray[i].charAt(j+1).toUpperCase();
+        //     }
+        //     if(newText.charAt(newText.length-1)!=="."){
+        //         newText+=" ";
+        //     }
+        // }
+        setText(newText);
+        props.showAlert("Converted to aLtErNaTiNg cAsE", "success");
+    }
+    const handleOnSentenceClick = () => {
+        let newText="";
+        console.log(sentencesArray);
+        for(var i=0; i<sentencesArray.length; i++){
+            newText+=sentencesArray[i].charAt(0).toUpperCase();
+            for(var j=1; j<sentencesArray[i].length; j++){
+                newText+=sentencesArray[i].charAt(j).toLowerCase();
+            }
+            newText+=". ";
+        }
+        setText(newText);
+        props.showAlert("Converted to Sentence case.", "success");
+    }
+    const handleOnChange = (event) => {
+        // console.log("On change");
+        setText(event.target.value);
+    }
+    const [text, setText]=useState("");
+    //text is called 'state variable'
+    // text="new text"; //Wrong way to update state
+    // setText("new text"); //Correct way to update state
+    let tempText=text.trim();
+    let wordsArray=tempText.split(" ");
+    for(var i=0; i<wordsArray.length; i++){
+        if(wordsArray[i]===""){
+            wordsArray.splice(i, 1);
+            i--;
+        }
+    }
+    let sentencesArray=tempText.split(".");
+    for(var i=0; i<sentencesArray.length; i++){
+        sentencesArray[i]=sentencesArray[i].trim();
+        if(sentencesArray[i]===""){
+            sentencesArray.splice(i, 1);
+            i--;
+        }
+    }
+    return (
+        <>
+            <div className="container" style={{color: props.mode==="light"?"black":"white"}}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} placeholder="Enter your text here" onChange={handleOnChange} id="myBox" rows="8" style={{backgroundColor: props.mode==="dark"?"#212529":"white", color:props.mode==="light"?"#212529":"white"}}></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleOnUpperClick}>Convert to UPPER CASE</button>
+                <button className="btn btn-primary mx-1" onClick={handleOnLowerClick}>Convert to lower case</button>
+                <button className="btn btn-primary mx-1" onClick={handleOnAlternatingClick}>Convert to aLtErNaTiNg cAsE</button>
+                <button className="btn btn-primary mx-1" onClick={handleOnSentenceClick}>Convert to Sentence case.</button>
+                <button className="btn btn-primary mx-1" onClick={handleOnClearClick}>Clear</button>
+            </div>
+            <div className="container my-3" style={{color: props.mode==="light"?"black":"white"}}>
+                <h2>Your text summary</h2>
+                <p><strong>{wordsArray.length}</strong> words and <strong>{tempText.length}</strong> characters</p>
+                <p><strong>{sentencesArray.length}</strong> sentences</p>
+                <p><strong>{Math.round(0.008 * wordsArray.length)}</strong> minutes read</p>
+                <h2>Preview</h2>
+                <p>{text.length>0?text:"Enter something in the text-box above to preview here"}</p>
+            </div>
+        </>
+  )
+}
